8a720aa7e3b6890ca2133c56046542b0c1b8e095
Sergey <sergo920@yahoo.com>
2020-04-12 15:20:45 +0300

    Create Schedule.php

---
+++ src/Enums/Schedule.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace MCS\Enums;
+/**
+ * Created by PhpStorm.
+ * User: sergo
+ * Date: 4/12/20
+ * Time: 2:14 PM
+ */
+class Schedule
+{
+    const EVERY_15_MINUTES = '_15_MINUTES_';
+    const EVERY_30_MINUTES = '_30_MINUTES_';
+    const EVERY_HOUR = '_1_HOUR_';
+    const EVERY_2_HOURS = '_2_HOURS_';
+    const EVERY_4_HOURS = '_4_HOURS_';
+    const EVERY_8_HOURS = '_8_HOURS_';
+    const EVERY_12_HOURS = '_12_HOURS_';
+    const EVERY_DAY = '_1_DAY_';
+    const EVERY_2_DAYS = '_2_DAYS_';
+    const EVERY_3_DAYS = '_72_HOURS_';
+    const EVERY_WEEK = '_1_WEEK_';
+    const EVERY_14_DAYS = '_14_DAYS_';
+    const EVERY_15_DAYS = '_15_DAYS_';
+    const EVERY_30_DAYS = '_30_DAYS_';
+    const DELETE_SCHEDULE = '_NEVER_';
+}
